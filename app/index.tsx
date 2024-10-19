5aa15a4a42c969e7c1e84fd56ca48f1ba1637e71
unknown <dannielbailey1999@gmail.com>
2024-10-18 20:54:11 -0500

    FlatList

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,23 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 import { Link } from "expo-router";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import FoodListItem from "../src/components/foodListItem";
+
+const foodItems = [
+  {label: 'Pizza', cal: 75, brand: 'Dominos'},
+  {label: 'Cake', cal: 75, brand: 'Case'},
+  {label: 'Biscuit', cal: 75, brand: 'Kisko'}
+]
 
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <FoodListItem item={{label: 'Pizza', cal: 75, brand: 'Dominos'}}/>
-      <FoodListItem item={{label: 'Cake', cal: 75, brand: 'Case'}}/>
+      <FlatList
+      data={foodItems}
+      renderItem={({item}) => <FoodListItem item={item}/>}
+      contentContainerStyle={{gap: 5}}
+      />
     </View>
   );
 }
