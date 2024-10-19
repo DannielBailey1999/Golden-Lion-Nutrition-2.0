f6c08d6dd3193ae3dec8e12013bfebe151546646
unknown <dannielbailey1999@gmail.com>
2024-10-18 20:47:46 -0500

    StyleSheets

--- src/components/foodListItem.tsx
+++ src/components/foodListItem.tsx
@@ -9,18 +9,10 @@ interface FoodListItemProps {
 
 const FoodListItem: React.FC<FoodListItemProps> = ({ item }) => {
   return (
-    <View style=
-    {{backgroundColor: 'gainsboro', 
-    padding: 10, 
-    borderRadius: 5, 
-    flexDirection: 'row', 
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    }}
-    >
+    <View style={styles.container}>
     <View style={{flex: 1, gap: 5}}>
-    <Text style={{fontWeight: 'bold', fontSize: 16}}>{item.label}</Text>
-    <Text style={{color: 'dimgray'}}>{item.cal} cal, {item.brand}</Text>
+    <Text style={styles.foodLabel}>{item.label}</Text>
+    <Text style={styles.fooddata}>{item.cal} cal, {item.brand}</Text>
     </View>
     <AntDesign name="pluscircleo" size={24} color="royalblue" />
 
@@ -28,5 +20,24 @@ const FoodListItem: React.FC<FoodListItemProps> = ({ item }) => {
     
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'gainsboro', 
+        padding: 10, 
+        borderRadius: 5, 
+        flexDirection: 'row', 
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    foodLabel: {
+        fontWeight: 'bold', 
+        fontSize: 16,
+    },
+    fooddata: {
+        fontWeight: 'bold', 
+        fontSize: 16
+    }
+})
 
 export default FoodListItem;
